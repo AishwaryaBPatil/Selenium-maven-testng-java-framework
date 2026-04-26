0df5d0e84a6d6cc45b091b92a93499ecf67725f0
Aishwarya <patilaishwarya9168@gmail.com>
2026-04-26 23:00:50 +0530

    fix file name extent report

--- src/test/java/Util/Extentreport.java
+++ src/test/java/Util/Extentreport.java
@@ -8,7 +8,7 @@ public class Extentreport {
 	
 	public static ExtentReports getExtentReports()
 	{
-	ExtentSparkReporter reporter=new ExtentSparkReporter("reports/ExtentReport.html");
+	ExtentSparkReporter reporter=new ExtentSparkReporter("reports/extentreport.html");
 	ExtentReports extent=new ExtentReports();
 	extent.attachReporter(reporter);
 	return extent;
